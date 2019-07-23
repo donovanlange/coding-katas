5551cbb806af4cd6d066032fbfd6a2aa720a2d81
Donovan Lange <dolange@microsoft.com>
2019-07-23 16:04:42 -0700

    Add UnitTests!

---
+++ algorithms.test/BubbleSortTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+using Algorithms.Sorting;
+
+namespace Algorithms.Sorting.Test
+{
+    public class BubbleSortTests
+    {
+        [Fact]
+        public void Sort_WithNullValues_ReturnsNullValues()
+        {
+            Assert.Null(BubbleSort.Sort(unordered: null));
+        }
+    }
+}
---
+++ algorithms/sorting/BubbleSort.cs
@@ -0,0 +1,15 @@
+namespace Algorithms.Sorting
+{
+    public static class BubbleSort
+    {
+        /// <summary>
+        /// Sorts the given array of numbers using a BubbleSort algorithm.
+        /// </summary>
+        /// <param name="unordered">The unordered values to be sorted.</param>
+        /// <returns>The sorted values.</returns>
+        public static int[] Sort(int[] unordered)
+        {
+            return unordered;
+        }
+    }
+}
