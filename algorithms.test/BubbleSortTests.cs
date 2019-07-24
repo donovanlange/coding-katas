18cb88264186c82a23ee031be4dc3b2bc33d129d
Donovan Lange <dolange@microsoft.com>
2019-07-23 19:40:23 -0700

    Mo unit tests, mo betta!

--- algorithms.test/BubbleSortTests.cs
+++ algorithms.test/BubbleSortTests.cs
@@ -1,4 +1,5 @@
 using Xunit;
+using System;
 
 namespace Algorithms.Sorting.Test
 {
@@ -10,11 +11,22 @@ namespace Algorithms.Sorting.Test
             Assert.Null(BubbleSort.Sort(values: null));
         }
 
-        [Fact]
-        public void Sort_ReturnsSortedValues()
+        private int[] Sorted(int[] values)
         {
-            Assert.Equal(new int[]{7, 8, 9}, BubbleSort.Sort(new int[] {9, 8, 7}));
+            int[] sortedValues = (int[]) values.Clone();
+            Array.Sort(sortedValues);
+            return sortedValues;
         }
 
+        [Theory]
+        [InlineData(new int[] {9, 8, 7})]
+        [InlineData(new int[] {7, 8, 9})]
+        [InlineData(new int[] {10, 7, 7, 7, 8, 9})]
+        [InlineData(new int[] {10, 9, 8, 7, 6, 4, 5, 3, 1, 2})]
+        [InlineData(new int[] {9})]
+        public void Sort_ReturnsSortedValues(int[] values)
+        {
+            Assert.Equal(Sorted(values), BubbleSort.Sort(values));
+        }
     }
 }
